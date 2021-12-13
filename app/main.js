4f6794b191566540470cbc8a8a4c0e921d52668b
agusfernandez <agustina.fer91@gmail.com>
2021-12-12 23:50:39 -0300

    nuevas secciones agregadas

--- app/main.js
+++ app/main.js
@@ -1,1 +1,38 @@
-document.write("hola");
+$(document).ready(function() {
+    $("#btn__hamburguer").click(function(){
+        if ($('.header').hasClass('open')){
+            $('.header').removeClass('open');
+            $('body').removeClass('noscroll');
+            $('.in__fade').removeClass('fade-in');
+            $('.in__fade').addClass('fade-out');
+            $('.line-hamburguer').removeClass('mobile__clic');
+        }else{
+            $('.header').addClass('open');
+            $('body').addClass('noscroll');
+            $('.in__fade').addClass('fade-in');
+            $('.in__fade').removeClass('fade-out');
+            $('.line-hamburguer').addClass('mobile__clic');
+ 
+        }
+    });
+
+    $(".mobile__slider").slick({
+        dots: false,
+        speed: 300,
+        slidesToShow: 0,
+        adaptiveHeight: true,
+        responsive: [
+            {
+              breakpoint: 500,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }
+          ]  
+    });
+
+
+
+});
+
